61e4e53a55a5b0d7abc76172163e6a817c277777
shivrajsharma <shivraj.sharma70@gmail.com>
2019-03-03 21:46:37 -0800

    finished weightedTotalStrategy and Missing class and some other

--- src/grading/Grade.java
+++ src/grading/Grade.java
@@ -2,8 +2,28 @@ package grading;
 
 public class Grade {
 
+	private String key;
+	private Double value;
+	public Grade(String key) {
+		
+	}
 	public Grade(String key, Double v) {
 		// TODO Auto-generated constructor stub
+	}
+	public Grade(String key, double v) {
+		// TODO Auto-generated constructor stub
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
 	}
 
 }
--- src/grading/WeightedTotalStrategy.java
+++ src/grading/WeightedTotalStrategy.java
@@ -5,25 +5,56 @@ import java.util.Map;
 
 public class WeightedTotalStrategy implements GradingStrategy {
 	
-	Map<String, Double> weights;
+	public Map<String, Double> weights;
 	public WeightedTotalStrategy()
 	{
-		
+		weights=null;
 	}
 
 	public WeightedTotalStrategy(Map<String, Double> courseWeights) {
 		// TODO Auto-generated constructor stub
+		weights=courseWeights;
 	}
 
 	@Override
 	public Grade calculate(String Key, List<Grade> grades) throws SizeException {
 		// TODO Auto-generated method stub
+		Missing missing=new Missing();
+		Grade grade=null;
 		if(grades ==null || grades.isEmpty()) 
 		{
 			throw new SizeException();
 		}
+		else if(weights == null)
+		{
+			grade =new Grade(Key,1.0);
+		}
 		
-		return null;
+		for (Grade g : grades) 
+		{
+			
+			if(weights.containsKey(g))
+			{		
+				Double weight=weights.get(Key);
+				if(weight==null)
+				{
+					
+					grade =  new Grade(Key,missing.doubleValue(weight, 1.0));
+				}
+				else if(weight <= 0.0)
+				{
+					grade = new Grade(Key,missing.doubleValue(null));
+				}
+				else if(g.getValue()==null)
+				{
+					grade = new Grade(Key,missing.doubleValue(g.getValue()));
+				}
+				
+			}
+			
+		}
+		
+		return grade;
 	}
 
 }
